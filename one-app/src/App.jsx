2388cb14609670d8ec273e9f17cd4446701bbc3e
Plycedes <abhaypratapyadav340@gmail.com>
2024-07-19 15:37:45 +0530

    Hooks

--- one-app/src/App.jsx
+++ one-app/src/App.jsx
@@ -1,5 +1,24 @@
+import { useState } from "react";
+
 function App() {
-    return <h1>Test</h1>;
+    let [counter, setCounter] = useState(0);
+
+    const addValue = () => {
+        setCounter(counter + 1);
+    };
+
+    const subValue = () => {
+        setCounter(counter - 1);
+    };
+    return (
+        <>
+            <h2>Value: {counter}</h2>
+            <button onClick={addValue}>Increase</button>
+            <br />
+            <br />
+            <button onClick={subValue}>Decrease</button>
+        </>
+    );
 }
 
 export default App;
