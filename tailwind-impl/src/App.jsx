bfdbe79a22253f273c625a46708ae51d721a0fd9
Plycedes <abhaypratapyadav340@gmail.com>
2024-07-21 01:41:47 +0530

    CHanges

--- tailwind-impl/src/App.jsx
+++ tailwind-impl/src/App.jsx
@@ -2,13 +2,13 @@ import { useState } from "react";
 import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 import "./App.css";
+import Card from "./components/Card";
 
 function App() {
-    const [count, setCount] = useState(0);
-
     return (
         <>
-            <h1 className="bg-green-400 text-black p-4 rounded-xl">Tailwind Test</h1>
+            <h1 className="bg-green-400 text-black p-4 rounded-xl mb-4">Tailwind Test</h1>
+            <Card name="Abhay Pratap Yadav" desc="The smart man alive" />
         </>
     );
 }
