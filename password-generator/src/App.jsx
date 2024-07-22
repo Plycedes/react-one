bc9f0a53d4efb6d8a8e1c6e93d9424daa8e4eb82
Plycedes <abhaypratapyadav340@gmail.com>
2024-07-22 23:55:21 +0530

    Set Password method

--- password-generator/src/App.jsx
+++ password-generator/src/App.jsx
@@ -1,35 +1,47 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState, useCallback } from "react";
 
 function App() {
-  const [count, setCount] = useState(0)
+    const [length, setLength] = useState(8);
+    const [iNum, setINum] = useState(false);
+    const [iChar, setIChar] = useState(false);
+    const [password, setPassword] = useState("");
 
-  return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    const passwordGenerator = useCallback(() => {
+        let pass = "";
+        let str = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+
+        if (iNum) str += "1234567890";
+        if (iChar) str += "!@#$%^&?*";
+
+        for (let i = 1; i <= length; i++) {
+            let char = Math.floor(Math.random() * str.length + 1);
+            pass += str.charAt(char);
+        }
+
+        setPassword(pass);
+    }, [length, iNum, iChar, setPassword]);
+
+    return (
+        <>
+            <div className="w-full max-w-md mx-auto shadow-md rounded-lg px-4 my-8 text-orange-500 bg-gray-700">
+                <h1 className="text-4xl text-center">Password Generator</h1>
+                <br />
+                <div className="flex shadow rounded-lg overflow-hidden mb-4">
+                    <input
+                        type="text"
+                        value={password}
+                        className="outline-none w-full py-1 px-3"
+                        placeholder="password"
+                        readOnly
+                    />
+                    <button className="outline-none bg-blue-700 text-white px-3 py-0.5 shrink-0">
+                        copy
+                    </button>
+                </div>
+            </div>
+        </>
+    );
 }
 
-export default App
+export default App;
+
