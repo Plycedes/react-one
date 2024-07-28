50b7594e8602a9f54234e8810574155c096e6d3f
Plycedes <abhaypratapyadav340@gmail.com>
2024-07-28 17:24:01 +0530

    Card and Theme Button

--- theme-controller/src/App.jsx
+++ theme-controller/src/App.jsx
@@ -1,5 +1,7 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import { ThemeProvider } from "./context/theme";
+import ThemeBtn from "./component/ThemeBtn";
+import Card from "./component/Card";
 
 function App() {
     const [themeMode, setThemeMode] = useState("light");
@@ -21,9 +23,13 @@ function App() {
         <ThemeProvider value={{ themeMode, lightTheme, darkTheme }}>
             <div className="flex flex-wrap min-h-screen items-center">
                 <div className="w-full">
-                    <div className="w-full max-w-sm mx-auto flex justify-end mb-4"></div>
+                    <div className="w-full max-w-sm mx-auto flex justify-end mb-4">
+                        <ThemeBtn />
+                    </div>
 
-                    <div className="w-full max-w-sm mx-auto"></div>
+                    <div className="w-full max-w-sm mx-auto">
+                        <Card />
+                    </div>
                 </div>
             </div>
         </ThemeProvider>
