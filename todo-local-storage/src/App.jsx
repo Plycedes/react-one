451a8d300c35b279c747eec7aacaecda662731ea
Plycedes <abhaypratapyadav340@gmail.com>
2024-07-29 21:37:26 +0530

    Project Complete

--- todo-local-storage/src/App.jsx
+++ todo-local-storage/src/App.jsx
@@ -1,20 +1,17 @@
 import { useState, useEffect } from "react";
 import { TodoProvider } from "./context/index";
+import { TodoForm, TodoItem } from "./components";
 //import './App.css'
 
 function App() {
     const [todos, setTodos] = useState([]);
 
     const addTodo = (todo) => {
-        setTodos((prev) => [{ id: Date.now, ...todo }, ...prev]);
+        setTodos((prev) => [{ id: Date.now(), ...todo }, ...prev]);
     };
 
     const updateTodo = (id, todo) => {
-        setTodos((prev) =>
-            prev.map((prevTodo) => {
-                prevTodo.id === id ? todo : prevTodo;
-            })
-        );
+        setTodos((prev) => prev.map((prevTodo) => (prevTodo.id === id ? todo : prevTodo)));
     };
 
     const deleteTodo = (id) => {
@@ -46,8 +43,16 @@ function App() {
             <div className="bg-[#172842] min-h-screen py-8">
                 <div className="w-full max-w-2xl mx-auto shadow-md rounded-lg px-4 py-3 text-white">
                     <h1 className="text-2xl font-bold text-center mb-8 mt-2">Manage Your Todos</h1>
-                    <div className="mb-4">{/* Todo form goes here */}</div>
-                    <div className="flex flex-wrap gap-y-3">{/*Loop and Add TodoItem here */}</div>
+                    <div className="mb-4">
+                        <TodoForm />
+                    </div>
+                    <div className="flex flex-wrap gap-y-3">
+                        {todos.map((todo) => (
+                            <div key={todo.id} className="w-full">
+                                <TodoItem todo={todo} />
+                            </div>
+                        ))}
+                    </div>
                 </div>
             </div>{" "}
         </TodoProvider>
